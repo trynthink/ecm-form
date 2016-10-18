4b282be18ccc4955f471d223eb082c900d5603b6
Chioke Harris <trynthink@gmail.com>
2016-10-18 10:41:44 -0400

    Generalize distribution selection
    Modify the jQuery code that adds the probability distribution type selection and the parameter fields to the DOM to be more general (not specific to the performance specification) in anticipation of the addition of installed cost and lifetime fields to the form.

--- form.js
+++ form.js
@@ -164,6 +164,58 @@ $(document).ready(function(){
     }
   }
 
+  function probFieldGenerator(distro_type, field_class) {
+    // Define probability distribution parameter fields to add to DOM
+    var prob_params = "<div id='" + field_class + "-prob-params'><div id='" + field_class + "-param-1'></div><input type='number' class='sm-text-input' name='distribution-param-1'><div id='" + field_class + "-param-2'></div><input type='number' class='sm-text-input' name='distribution-param-2'></div></div>";
+
+    // Set the parameter field text labels accordingly
+    if (distro_type === "uniform") {
+      var p1name = "Minimum";
+      var p2name = "Maximum";
+    }
+    else if (distro_type === "normal") {
+      var p1name = "Mean";
+      var p2name = "Standard Deviation";
+    }
+    else if (distro_type === "lognormal") {
+      var p1name = "Mean";
+      var p2name = "Standard Deviation";
+    }
+    else if (distro_type === "gamma") {
+      var p1name = "Shape (k)";
+      var p2name = "Scale (&#952;)"; // theta
+    }
+    else if (distro_type === "weibull") {
+      var p1name = "Shape (k)";
+      var p2name = "Scale (&#955;)"; // lambda
+    }
+    else { // triangular
+      var p1name = "Lower limit";
+      var p2name = "Mode";
+      var p3name = "Upper limit";
+    }
+
+    // Add probability distribution parameter fields
+    $("#" + field_class + "-prob-distro").append(prob_params);
+    
+    // Add correct field labels for the distribution type
+    $("#" + field_class + "-param-1").append(p1name);
+    $("#" + field_class + "-param-2").append(p2name);
+
+    // Add special field for triangular distribution
+    if (distro_type === "triangular") {
+      $("#" + field_class + "-prob-params").append("<div id='" + field_class + "-param-3'></div><input type='number' class='sm-text-input' name='distribution-param-3'>");
+      $("#" + field_class + "-param-3").append(p3name);
+    }
+  }
+
+  // Insert probability dropdown menu for the specified field class
+  // i.e., "cost", "perf", or "life"
+  function configProbDropdown(field_class) {
+    var prob_menu = "<div id='" + field_class + "-prob-distro'><select name='" + field_class + "-distro'><option selected disabled hidden style='display: none' value=''>Distribution Options</option><option value='uniform'>Uniform</option><option value='normal'>Normal</option><option value='lognormal'>Log-normal</option><option value='gamma'>Gamma</option><option value='weibull'>Weibull</option><option value='triangular'>Triangular</option></select></div>";
+    return prob_menu
+  }
+
 
 
   ///////////////////////////////////////////////////////////////////////////
@@ -172,11 +224,6 @@ $(document).ready(function(){
 
   // Define HTML content for unitary value input
   var perf_unitary = "<label>Value <input type='number' class='sm-text-input' name='perf-unitary'></label>"
-
-  // Define HTML content for probability distribution
-  var prob_select = "<div id='prob-distro'><select name='distribution-type'><option selected disabled hidden style='display: none' value=''>Distribution Options</option><option value='uniform'>Uniform</option><option value='normal'>Normal</option><option value='lognormal'>Log-normal</option><option value='gamma'>Gamma</option><option value='weibull'>Weibull</option><option value='triangular'>Triangular</option></select></div>";
-
-  var prob_params ="<div id='prob-params'><div id='param-1'></div><input type='number' class='sm-text-input' name='distribution-param-1'><div id='param-2'></div><input type='number' class='sm-text-input' name='distribution-param-2'></div></div>";
 
   // On page load, add default performance field (single value) and check
   // the appropriate radio button
@@ -197,7 +244,7 @@ $(document).ready(function(){
       $("#perf-spec").append(perf_unitary);
     }
     else if (perf_spec_type === "spec-by-prob") {
-      $("#perf-spec").append(prob_select);
+      $("#perf-spec").append(configProbDropdown("perf"));
     }
     else {
       cplFieldGenerator(perf_spec_type, "#perf-spec", "perf", "number");
@@ -207,54 +254,20 @@ $(document).ready(function(){
     updateSourceFields("#perf-source", "perf");
   });
 
-  ///////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
+  // DISTRIBUTION TYPE SELECTION
 
   // Respond to distribution type selection
-  $(document).on("change", "select[name=distribution-type]", function(){
+  $(document).on("change", "select[name=perf-distro]", function(){
     // Clear existing entries in the area
-    $("#prob-params").remove();
+    $("#perf-prob-params").remove();
 
     // Get the dropdown menu selection
-    var perf_distro_type = $("select[name=distribution-type] option:selected").val();
-    // Set the parameter values accordingly
-    if (perf_distro_type === "uniform") {
-      var p1name = "Minimum";
-      var p2name = "Maximum";
-    }
-    else if (perf_distro_type === "normal") {
-      var p1name = "Mean";
-      var p2name = "Standard Deviation";
-    }
-    else if (perf_distro_type === "lognormal") {
-      var p1name = "Mean";
-      var p2name = "Standard Deviation";
-    }
-    else if (perf_distro_type === "gamma") {
-      var p1name = "Shape (k)";
-      var p2name = "Scale (&#952;)"; // theta
-    }
-    else if (perf_distro_type === "weibull") {
-      var p1name = "Shape (k)";
-      var p2name = "Scale (&#955;)"; // lambda
-    }
-    else { // triangular
-      var p1name = "Lower limit";
-      var p2name = "Mode";
-      var p3name = "Upper limit";
-    }
+    var perf_distro_type = $("select[name=perf-distro] option:selected").val();
 
-    // Add probability distribution parameter fields
-    $("#prob-distro").append(prob_params);
-    
-    // Add correct field labels
-    $("#param-1").append(p1name);
-    $("#param-2").append(p2name);
-
-    // Add special fields for triangular distribution
-    if (perf_distro_type === "triangular") {
-      $("#prob-params").append("<div id='param-3'></div><input type='number' class='sm-text-input' name='distribution-param-3'>");
-      $("#param-3").append(p3name);
-    }
+    // Call function to determine parameter names and add appropriate
+    // fields to the DOM to specify the distribution parameters
+    probFieldGenerator(perf_distro_type, "perf");
   });
 
   ///////////////////////////////////////////////////////////////////
